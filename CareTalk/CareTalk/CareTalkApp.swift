8d0da22d4a25a8b6c399cf3c36ef099aa7aef71c
PapaDevilno <94031852+PapaDevilno@users.noreply.github.com>
2023-10-20 08:32:15 +0700

    Create Xcode File
    -w-

---
+++ CareTalk/CareTalk/CareTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CareTalkApp.swift
+//  CareTalk
+//
+//  Created by Nicholas Yvees on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CareTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
